b8e698f4a3874b77d710fcecc98705186030941e
Gustavo Lora <132119031+Gustavo-045@users.noreply.github.com>
2024-03-05 11:18:00 -0500

    Update main.c

--- RetoFinal/main.c
+++ RetoFinal/main.c
@@ -1,67 +1,58 @@
+enum class Task1States {
+    CONFIG,
+    WAIT_DATA
+};
+
+static Task1States task1State = Task1States::CONFIG;
+static uint8_t TiempoParaAbrir = 5;
+
 void task1()
 {
-    enum class Task1States    {
-        CONFIG,
-        WAIT_DATA
-    };
-    static Task1States task1State = Task1States::CONFIG;
-    static uint8_t TiempoParaAbrir = 5;
-
     switch (task1State)
     {
     case Task1States::CONFIG:
     {
         char TeclaRecibida;
-        
-        Serial.begin(115200);
-        Serial.print("CONFIG");
-        Serial.print("Tiempo para abrir la boveda: " + TiempoParaAbrir);
-        Serial.print("Presiona S para aumentar la cantidad de segundos y A para disminuirla, Presiona L para aceptar los cambios");
 
-        
+
+        /*Serial.print("CONFIG - Tiempo para abrir la boveda: ");
+        Serial.println(TiempoParaAbrir);
+        Serial.println("Presiona S para aumentar la cantidad de segundos y A para disminuirla, Presiona L para aceptar los cambios");
+        Serial.begin(115200);
+        */
         
         if (Serial.available() > 0)
         {
-          TeclaRecibida = Serial.read();
+    
+            TeclaRecibida = Serial.read();
 
-          
-           if (TeclaRecibida != 'L')
-           {
-                  
-              if (TeclaRecibida == 'S')
-              {
-                TiempoParaAbrir++;
-                Serial.print("Tiempo para abrir la boveda: " + TiempoParaAbrir);
-              }
-              if (TeclaRecibida == 'A')
-              {
-                TiempoParaAbrir--;
-                Serial.print("Tiempo para abrir la boveda: " + TiempoParaAbrir);
-                
-              }
-              
-           }
-
-           else
-           {
-            Serial.print("Listo");
-            task1State = Task1States::WAIT_DATA;
-           }
-
-           
-
-          
+            if (TeclaRecibida != 'L')
+            {
+                if (TeclaRecibida == 'S')
+                {
+                    TiempoParaAbrir++;
+                    Serial.println("Tiempo para abrir la boveda: " + String(TiempoParaAbrir));
+                }
+                else if (TeclaRecibida == 'A')
+                {
+                    TiempoParaAbrir--;
+                    Serial.println("Tiempo para abrir la boveda: " + String(TiempoParaAbrir));
+                }
+            }
+            else
+            {
+                Serial.println("Listo");
+                task1State = Task1States::WAIT_DATA;
+            }
         }
 
-        
-        
         break;
     }
 
     case Task1States::WAIT_DATA:
     {
-        // evento 1:        
-        
+        // Evento 1:
+
         break;
     }
 
@@ -72,7 +63,6 @@ void task1()
     }
 }
 
-
 void setup()
 {
     task1();
@@ -80,5 +70,6 @@ void setup()
 
 void loop()
 {
-
+    task1();
+    // Aquí puedes agregar otras tareas que se ejecuten en el bucle principal si es necesario.
 }
